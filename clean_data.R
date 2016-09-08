27b87f6df6cc9d168a3ed392b69fe7ac8cd6f53c
Thomas McMahon <thmcmahon@gmail.com>
2016-09-08 13:37:13 +1000

    Removed magic numbers and fixed comments

--- clean_data.R
+++ clean_data.R
@@ -1,32 +1,56 @@
 library(dplyr)
 
 clean_data <- function() {
+  # A function to normalise and clean the following dataset:
+  #
+  # Australian Census Longitudinal Dataset, 2006-2011
+  # 
+  # Sex 2006, Age 2006, Labour Force Status 2006 and Total Personal Income
+  # (weekly) 2006 by Persons (weighted), Labour Force Status 2011 and Total
+  # Personal Income (weekly) 2011 
+  # 
+  # Counting: Persons (weighted)
+  # 
+  # This data can't be published as it's confidential. To gain access contact
+  # the ABS.
+  #
   file <- 'data_raw/super_v6.csv'
   
   df <- read.csv(file, header = FALSE, stringsAsFactors = FALSE, skip = 5,
                  na.strings = "")
   
   # Junk columns and rows with ABS metadata
-  df <- df[1:580,-1]
+  actual_data <- 1:580
+  metadata_col <- 1
+  df <- df[actual_data,-metadata_col]
   
-  # combine the 2nd and 3rd rows for the column name
   # na.locf fills NAs with last available name
-  names_vector <- paste(zoo::na.locf(t(df[2,]), na.rm = FALSE), t(df[3,]),
-                        sep = "_")
+  lf_names <- 2
+  income_names <- 3
+  names_vector <- paste(zoo::na.locf(
+    t(df[lf_names,]), na.rm = FALSE),
+    t(df[income_names,]),
+    sep = "_")
   
   # fix the names
   names_vector <- gsub(" *\\(.*?\\) *", "", names_vector) # annual totals
-  names_vector <- gsub("\\$", "", names_vector) # dollar signs
-  names_vector <- gsub("Employed_", "e_", names_vector) # nice short names
+  # nice short names
+  names_vector <- gsub("Employed_", "e_", names_vector) 
   names_vector <- gsub("Not in the labour force_", "nilf_", names_vector)
   names_vector <- gsub("Unemployed_", "u_", names_vector)
+  # junk chars
+  names_vector <- gsub("\\$", "", names_vector)
   names_vector <- gsub("\\,", "", names_vector)
   names_vector <- gsub(" ", "_", names_vector)
   
   names(df) <- names_vector
   
-  # demerge and fill
-  df[4:nrow(df),1:3] <- sapply(df[4:nrow(df),1:3], zoo::na.locf)
+  # Normalise the column variables which
+  col_data_starts <- 4
+  col_vars <- 1:3
+  df[col_data_starts:nrow(df),col_vars] <- sapply(df[col_data_starts:nrow(df),
+                                                     col_vars],
+                                             zoo::na.locf)
   
   # remove junk empty rows now that the names have been added
   df <- df[5:nrow(df),]
@@ -55,7 +79,9 @@ clean_data <- function() {
                       -lf_status_2006, -weekly_income_2006)
   
   # convert strings to floats for the counts
-  df[,4:ncol(df)] <- sapply(df[,4:ncol(df)], as.numeric)
+  numeric_vars_start <- 4
+  df[,numeric_vars_start:ncol(df)] <- sapply(df[,numeric_vars_start:ncol(df)],
+                                             as.numeric)
   
   saveRDS(df, file = 'data/census_super_data.Rds')
 }
